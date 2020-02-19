868945b371862c8609db152f798b43fe6d5e87f8
Abdulrasaq Saliu <asaliu@AS-MBP2018.local>
2020-02-19 00:35:22 -0500

    Created the UI for the Login Page and added the functionality of the Enter button

---
+++ ThePlug/app/src/main/java/com/example/theplug/HomeScreen.java
@@ -0,0 +1,24 @@
+package com.example.theplug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView1);
+        textView.setText(message);
+    }
+}
