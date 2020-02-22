1298b6285f1ddfd45120147285365f172dd76567
John <jtstone@buffalo.edu>
2020-02-22 15:22:31 -0500

    profile page and default profile picture main page links to profile page for now

--- ThePlug/app/src/main/java/com/example/theplug/MainActivity.java
+++ ThePlug/app/src/main/java/com/example/theplug/MainActivity.java
@@ -4,10 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.EditText;
-
-
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -20,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, HomeScreen.class);
+        Intent intent = new Intent(this, ProfileActivity.class);
 //        EditText editText = (EditText) findViewById(R.id.Username);
 //        String message = editText.getText().toString();
 //        intent.putExtra(EXTRA_MESSAGE, message);
