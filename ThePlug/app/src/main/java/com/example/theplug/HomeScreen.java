8c8eb01fecf53d69f8df6489c6cd66ad2a921984
Abdulrasaq Saliu <asaliu@AS-MBP2018.fios-router.home>
2020-02-23 01:02:43 -0500

    implemented the UI of the toolbar

--- ThePlug/app/src/main/java/com/example/theplug/HomeScreen.java
+++ ThePlug/app/src/main/java/com/example/theplug/HomeScreen.java
@@ -1,24 +1,40 @@
 package com.example.theplug;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeScreen extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView1);
-        textView.setText(message);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item)
+//        int id = item.getItemId();
+//
+//        if(id == R.id.account)
+//            Toast.makeText(getApplica)
+//    }
 }
