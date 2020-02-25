14a5038fefae302a0393c88d074105d0cbd4f257
Abdulrasaq Saliu <asaliu@AS-MBP2018.local>
2020-02-25 16:37:45 -0500

    add the search bar and the design the homepage UI

--- ThePlug/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ ThePlug/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f070025;
-        public static final int end = 0x7f070040;
-        public static final int gone = 0x7f070047;
-        public static final int invisible = 0x7f070055;
-        public static final int left = 0x7f07005a;
-        public static final int packed = 0x7f07006d;
-        public static final int parent = 0x7f07006e;
-        public static final int percent = 0x7f070070;
-        public static final int right = 0x7f070076;
-        public static final int spread = 0x7f070090;
-        public static final int spread_inside = 0x7f070091;
-        public static final int start = 0x7f070096;
-        public static final int top = 0x7f0700a9;
-        public static final int wrap = 0x7f0700b3;
+        public static final int end = 0x7f070045;
+        public static final int gone = 0x7f07004c;
+        public static final int invisible = 0x7f07005e;
+        public static final int left = 0x7f070063;
+        public static final int packed = 0x7f070078;
+        public static final int parent = 0x7f070079;
+        public static final int percent = 0x7f07007b;
+        public static final int right = 0x7f070081;
+        public static final int spread = 0x7f07009c;
+        public static final int spread_inside = 0x7f07009d;
+        public static final int start = 0x7f0700a2;
+        public static final int top = 0x7f0700b8;
+        public static final int wrap = 0x7f0700c2;
     }
     public static final class styleable {
         private styleable() {}
