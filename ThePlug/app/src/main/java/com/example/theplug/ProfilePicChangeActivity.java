e1c732c88bc96079affc7daf47429f71034f8176
Toyosi Oladepo <horehpoh001@yahoo.com>
2020-03-05 02:27:26 -0500

    Linked profile pic change page to button on profile page

--- ThePlug/app/src/main/java/com/example/theplug/ProfilePicChangeActivity.java
+++ ThePlug/app/src/main/java/com/example/theplug/ProfilePicChangeActivity.java
@@ -1,4 +1,14 @@
 package com.example.theplug;
 
-public class ProfilePicChangeActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfilePicChangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_pic_change);
+    }
 }
