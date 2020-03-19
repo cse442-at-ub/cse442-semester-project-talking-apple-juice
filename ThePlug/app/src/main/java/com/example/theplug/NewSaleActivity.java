a804c3e1999c456d470d65160f9f5d11c7ffff3d
Abdulrasaq Saliu <asaliu@AS-MBP2018.fios-router.home>
2020-03-18 23:48:06 -0400

    Testng

--- ThePlug/app/src/main/java/com/example/theplug/NewSaleActivity.java
+++ ThePlug/app/src/main/java/com/example/theplug/NewSaleActivity.java
@@ -1,14 +1,51 @@
 package com.example.theplug;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.UUID;
+
 public class NewSaleActivity extends AppCompatActivity {
+
+
+    private ProgressDialog pDialog;
+    JSONparsar jsonP = new JSONpar
+
+    EditText editName, editPrice, editType;
+    Button putforSale,  putforBid;
+    ImageView prodView;
+    private Uri prodImage;
+    private static final int GalleryPick = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +57,67 @@ public class NewSaleActivity extends AppCompatActivity {
             setTheme(R.style.darkTheme);
         }
         setContentView(R.layout.activity_new_sale);
+
+
+        //initialize variables
+        editName = (EditText) findViewById(R.id.editText4);
+        editPrice = (EditText) findViewById(R.id.editText2);
+        editType = (EditText) findViewById(R.id.editText6);
+        putforSale =(Button) findViewById(R.id.button3);
+        putforBid = (Button) findViewById(R.id.button4);
+        prodView  =  (ImageView)  findViewById(R.id.imageView7);
+
+
     }
+
+    private byte[] imageViewToByte(ImageView image){
+        Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] bytesArray = stream.toByteArray();
+        return bytesArray;
+    }
+
+    // Allows user to choose image from gallery regardless of device after clicking image
+    public void upLoader(View v) {
+        Intent gallaryIntent = new Intent();
+        gallaryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        gallaryIntent.setType("image/*");
+        startActivityForResult(gallaryIntent, GalleryPick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode == GalleryPick && resultCode == RESULT_OK){
+            prodImage  = data.getData();
+            prodView.setImageURI(prodImage);
+
+
+        }
+    }
+
+//    private void uploadImage(){
+//        FirebaseStorage storage = FirebaseStorage.getInstance();
+//        StorageReference storageRef = storage.getReference();
+//        StorageReference mountainsRef = storageRef.child("images");
+//
+//
+//
+//        prodView.setDrawingCacheEnabled(true);
+//        prodView.buildDrawingCache();
+//        Bitmap bitmap = ((BitmapDrawable) prodView.getDrawable()).getBitmap();
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+//        byte[] data = baos.toByteArray();
+//
+//        UploadTask uploadTask = mountainsRef.putBytes(data);
+//        Uri file = Uri.fromFile(new File("image"));
+//        StorageReference riversRef = mountainsRef.child("images/"+file.getLastPathSegment());
+//        uploadTask = riversRef.putFile(file);
+//
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -62,4 +159,5 @@ public class NewSaleActivity extends AppCompatActivity {
         }
         return false;
     }
+
 }
