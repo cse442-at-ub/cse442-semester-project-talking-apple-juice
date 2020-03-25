ec4399db9ccc71fa4eab8159021381310150f0f0
Toyosi Oladepo <horehpoh001@yahoo.com>
2020-03-25 05:53:45 -0400

    Added code for functionality on the "forgot username" page

--- ThePlug/app/src/main/java/com/example/theplug/ForgotUsernameActivity.java
+++ ThePlug/app/src/main/java/com/example/theplug/ForgotUsernameActivity.java
@@ -1,11 +1,16 @@
 package com.example.theplug;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 public class ForgotUsernameActivity extends AppCompatActivity {
+
+    private EditText getUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,27 @@ public class ForgotUsernameActivity extends AppCompatActivity {
             setTheme(R.style.darkTheme);
         }
         setContentView(R.layout.activity_forgot_username);
+
+        getUsername = findViewById(R.id.unEmailEditText4);
+        getUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retrieveUn(v);
+            }
+        });
+    }
+
+    public void retrieveUn(View v){ //REMEMBER TO IMPLEMENT WITH DATABASE
+        getUsername = findViewById(R.id.unEmailEditText4);
+        if(getUsername.getText().toString().equals("aosaliu@buffalo.edu") || getUsername.getText().toString().equals("ooladepo@buffalo.edu") || getUsername.getText().toString().equals("jtstone@buffalo.edu")){
+            getUsername.getText().clear();
+            Toast success = Toast.makeText(getApplicationContext(), "Account Found! Your username has been sent to your email", Toast.LENGTH_SHORT);
+            success.show();
+        }else {
+            getUsername.getText().clear();
+            Toast success = Toast.makeText(getApplicationContext(), "Account Not Found! Please enter a valid email", Toast.LENGTH_SHORT);
+            success.show();
+        }
+
     }
 }
